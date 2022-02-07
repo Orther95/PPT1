437009cc5beebf076a3bce94cb127e27c4f7c57b
Orther95 <Juanra.95@hotmail.es>
2022-02-07 18:58:37 +0100

    Poniendo las opciones en español del PPT

--- PPT_GAME.py
+++ PPT_GAME.py
@@ -7,3 +7,12 @@ print("1) Español")
 print("2) Ingles")
 idioma = int(input("Seleccione su idioma: "))
 # 1 
+if idioma == 1:
+    print("Juego en Español")
+    aleatorio = random.randrange(0, 3)
+    elijePc = ""
+    print("1)Piedra")
+    print("2)Papel")
+    print("3)Tijera")
+    opcion = int(input("Que elijes: "))
+    # 2
